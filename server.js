27cf41ccf2a25f738e40963cb00211b88f4d1c34
Estefanya <estefanya.nascimento@outlook.com>
2021-12-11 11:05:03 -0300

    atuali

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./src/app");
-const port = process.env.PORT
+const port = process.env.PORT || 9090;
 
 app.listen(process.env.PORT || port, () => {
-  console.log(`Viver sem limites pelo mundo ${port}`);
-});
+console.log(`Viver sem limites pelo mundo ${port}`)
+
+})
