9578c4b88547a2876dfabf1a41b4ebefb220c727
Estefanya <estefanya.nascimento@outlook.com>
2021-12-15 23:46:03 -0300

    merge

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app.js");
-const PORT= process.env.PORT || 6066;
+const PORT= process.env.PORT || 6070;
 
 app.listen(process.env.PORT || PORT, () => {
 console.log(`Viver sem limites pelo mundo ${PORT}`)
