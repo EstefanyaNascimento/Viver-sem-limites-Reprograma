219033ff01eaea5e811b33d9f9a1af573645f81a
Estefanya <estefanya.nascimento@outlook.com>
2021-12-16 10:52:24 -0300

    atualizacao .env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app.js");
-const PORT= process.env.PORT || 5556;
+const PORT= process.env.PORT || 5558;
 
 app.listen(process.env.PORT || PORT, () => {
 console.log(`Viver sem limites pelo mundo ${PORT}`)
